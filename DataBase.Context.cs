a7c39009c42f8573c5b0514d4b22d13594b1856e
Никита <magfox2017@gmail.com>
2024-05-30 10:52:05 +1000

    connect databases

--- DataBase.Context.cs
+++ DataBase.Context.cs
@@ -15,9 +15,16 @@ namespace NauchnaiConference
     
     public partial class NauchnaiConferenciiEntities : DbContext
     {
+        public static NauchnaiConferenciiEntities _context;
         public NauchnaiConferenciiEntities()
             : base("name=NauchnaiConferenciiEntities")
         {
+        }
+        public static NauchnaiConferenciiEntities GetContext()
+        {
+            if (_context == null)
+                _context = new NauchnaiConferenciiEntities();
+            return _context;
         }
     
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
